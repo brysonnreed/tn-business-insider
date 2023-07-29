2871f98b3fbd6da3e26a7f69b0d8bebaa8b12b3f
brysonnreed <Brysonjreed@gmail.com>
2023-07-29 01:08:58 -0500

    update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,7 +13,7 @@ export const authOptions = {
     //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     // }),
   ],
-  secret: process.env.NEXT_PUBLIC_SECRET,
+  secret: process.env.SECRET,
 }
 
 export default NextAuth(authOptions)
